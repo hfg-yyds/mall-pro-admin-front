ff835e2f9981b40a34cc646e14e6313a3e2d71e0
hfg-yyds <1841814080@qq.com>
2022-06-20 14:24:29 +0800

    菜单

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,7 +1,50 @@
-<!--三级分类维护-->
 <template>
-  <div class="">三级分类维护</div>
-  
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <!--  添加判断  v-if="node.level <= 2"   -->
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -12,17 +55,115 @@ export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
   data() {
-    //这里存放数据
-    return {};
+    return {
+      menus: [],
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+      },
+      expandedKey: [],
+      dialogVisible: false,
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+
+    //获取菜单的树形结构
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then((data) => {
+        console.log("成功获取菜单数据...", data.data.data);
+        this.menus = data.data.data;
+      });
+    },
+    // 添加三级分类
+    addCategory() {
+      console.log("添加三级分类", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      })
+        .then((data) => {
+          //关闭对话框
+          this.dialogVisible = false;
+          this.$message({
+            type: "success",
+            message: "菜单保存成功!",
+          });
+          //刷新数据
+          this.getMenus();
+          //设置删除之后展开的菜单
+          this.expandedKey = [this.category.parentCid];
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "保存失败!",
+          });
+        });
+    },
+    append(data) {
+      console.log("append", data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+    },
+
+    remove(node, data) {
+      this.$confirm(`此操作将永久删除${data.name}分类, 是否继续?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          console.log("remove", data);
+          // 为什么这样可以直接拿到ids
+          var ids = [data.catId];
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then((data) => {
+            console.log("删除成功");
+            this.getMenus();
+          });
+
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+
+          //设置删除之后展开的菜单
+          this.expandedKey = [node.parent.data.catId];
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
-  //方法集合
-  methods: {},
   //生命周期 - 创建完成(可以访问当前this实例)
-  created() {},
+  created() {
+    this.getMenus();
+  },
   //生命周期 - 挂载完成(可以访问DOM元素)
   mounted() {},
   beforeCreate() {}, //生命周期 - 创建之前
@@ -31,7 +172,7 @@ export default {
   updated() {}, //生命周期 - 更新之后
   beforeDestroy() {}, //生命周期 - 销毁之前
   destroyed() {}, //生命周期 - 销毁完成
-  activated() {}, //如果页面有keep-alive缓存功能,这个函数会触发
+  activated() {}, //页面被激活,就会触发这个方法,如果页面有keep-alive缓存功能,这个函数会触发
 };
 </script>
 <style lang='scss' scoped>
